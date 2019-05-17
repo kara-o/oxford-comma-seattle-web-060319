7a10c6a01baece3d9dfcaac6c6569f17a577768a
Kara O <karajo.odle@gmail.com>
2019-05-17 01:00:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,7 +10,7 @@ def oxford_comma(array)
     
   else
     new_array = array.slice(0..(array.length - 2))
-    new_array.join(', ') + ', and' + array.slice(-1)
+    new_array.join(', ') + ', and ' + array.slice(-1)
   end
   
 end
