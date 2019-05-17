ca39207770a9e4da4f6fbcfa58d5db714bddcd67
Kara O <karajo.odle@gmail.com>
2019-05-17 00:40:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,14 @@
 def oxford_comma(array)
-  array.join()
-  array.join(' and ')
+  if array.length == 1
+    array.join()
+  end
+  
+  if array.length == 2
+    array.join(' and ')
+  end
+  
+  if array.length == 3
+    array[0, 1].join(', ') + 'and ' + array[2].join()
+  
   
 end
